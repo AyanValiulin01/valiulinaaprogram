496e07694aa0842df6539b720585253b2a533ee0
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 18:56:20 +0600

    Update App.tsx

--- src/4/4_1_1/App.tsx
+++ src/4/4_1_1/App.tsx
@@ -3,17 +3,16 @@
   Если ввести сообщение и нажать "Отправить" то перед появлением сообщения "Отправлено!" произойдет трехсекундная задержка. Кнопка "Отменить" должна остановить появление сообщения "Отправлено!". Она делает это, вызывая clearTimeout для идентификатора таймаута, сохраненного во время handleSend. Однако даже после нажатия кнопки "Отменить" сообщение "Отправлено!" все равно появляется. Найдите причину неработоспособности и устраните ее.
 */
 
-import { useState } from 'react';
-import { getImageUrl } from "./utils"
+import { useState, useRef } from 'react';
 
 export default function Chat() {
   const [text, setText] = useState('');
   const [isSending, setIsSending] = useState(false);
-  let timeoutID = null;
+  const timeoutID = useRef<ReturnType<typeof setTimeout> | null>(null); 
 
   function handleSend() {
     setIsSending(true);
-    timeoutID = setTimeout(() => {
+    timeoutID.current = setTimeout(() => {
       alert('Отправлено!');
       setIsSending(false);
     }, 3000);
@@ -21,7 +20,10 @@ export default function Chat() {
 
   function handleUndo() {
     setIsSending(false);
-    clearTimeout(timeoutID);
+    if (timeoutID.current) {
+      clearTimeout(timeoutID.current); 
+      timeoutID.current = null;
+    }
   }
 
   return (
