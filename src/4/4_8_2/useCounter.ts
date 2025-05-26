4d67a788d4941849998c532484fbfc76b7d71b33
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 19:12:21 +0600

    Update useCounter.ts

--- src/4/4_8_2/useCounter.ts
+++ src/4/4_8_2/useCounter.ts
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 
-export function useCounter() {
+export function useCounter(delay: number) {
   const [count, setCount] = useState(0);
+
   useEffect(() => {
     const id = setInterval(() => {
       setCount(c => c + 1);
-    }, 1000);
+    }, delay);
+
     return () => clearInterval(id);
-  }, []);
+  }, [delay]); // Зависимость обновляется при изменении delay
+
   return count;
 }
