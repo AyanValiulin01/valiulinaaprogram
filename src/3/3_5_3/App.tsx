e917843d2747dce158a7b8666fd66298c9277d9a
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 18:49:43 +0600

    Update App.tsx

--- src/3/3_5_3/App.tsx
+++ src/3/3_5_3/App.tsx
@@ -1,33 +1,22 @@
-// 3_5_2 Clear the input on sending a message
 /*
-  В настоящее время нажатие кнопки "Отправить" ничего не делает. Добавьте обработчик события для кнопки "Отправить", который будет:
+  В этом примере переключение между разными получателями всегда очищает текстовый ввод. Это связано с тем, что вы не хотите разделять черновик одного сообщения между несколькими получателями. Но было бы лучше, если бы ваше приложение "запоминало" черновики для каждого контакта отдельно, восстанавливая их при переключении контактов.
 
-  - Показывать alert с электронной почтой получателя и сообщением.
-  - Очищать вход сообщения.
-*/
-
-import { useReducer } from 'react';
+  Ваша задача - изменить структуру состояния таким образом, чтобы запоминать  отдельный черновик сообщения для каждого контакта. Вам потребуется внести несколько изменений в редуктор, начальное состояние и компоненты.
+    
+*/import { useReducer } from 'react';
 import Chat from './Chat';
 import ContactList from './ContactList';
 import { initialState, messengerReducer } from './messengerReducer';
 
-export default function Messenger() {
+export default function App() {
   const [state, dispatch] = useReducer(messengerReducer, initialState);
-  const message = state.message;
   const contact = contacts.find((c) => c.id === state.selectedId)!!;
+  const message = state.messages[state.selectedId] || ''; // Загружаем черновик для выбранного контакта
+
   return (
     <div>
-      <ContactList
-        contacts={contacts}
-        selectedId={state.selectedId}
-        dispatch={dispatch}
-      />
-      <Chat
-        key={contact.id}
-        message={message}
-        contact={contact}
-        dispatch={dispatch}
-      />
+      <ContactList contacts={contacts} selectedId={state.selectedId} dispatch={dispatch} />
+      <Chat key={contact.id} message={message} contact={contact} dispatch={dispatch} />
     </div>
   );
 }
@@ -36,10 +25,10 @@ export type Contact = {
   id: number;
   name: string;
   email: string;
-}
+};
 
 const contacts = [
-  {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
-  {id: 1, name: 'Alice', email: 'alice@mail.com'},
-  {id: 2, name: 'Bob', email: 'bob@mail.com'},
+  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' },
 ];
