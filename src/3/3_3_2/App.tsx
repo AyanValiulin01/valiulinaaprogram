2ea3a23b8dd1cf8d38ffd416ee35e5176c46fe5c
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 18:42:29 +0600

    Update App.tsx

--- src/3/3_3_2/App.tsx
+++ src/3/3_3_2/App.tsx
@@ -1,3 +1,4 @@
+
 // 3_3_2 Filtering a list  
 /*
   В этом примере SearchBar имеет собственное состояние query, которое управляет вводом текста. Его родительский компонент FilterableList отображает список элементов, но он не учитывает поисковый запрос.
@@ -11,28 +12,24 @@ import { useState } from 'react';
 import { foods, filterItems, Item } from './data';
 
 export default function FilterableList() {
+  const [query, setQuery] = useState('');
+
   return (
     <>
-      <SearchBar />
+      <SearchBar query={query} setQuery={setQuery} />
       <hr />
-      <List items={foods} />
+      <List items={filterItems(foods, query)} />
     </>
   );
 }
 
-function SearchBar() {
-  const [query, setQuery] = useState('');
-
-  function handleChange(e: any) {
-    setQuery(e.target.value);
-  }
-
+function SearchBar({ query, setQuery }: { query: string, setQuery: (value: string) => void }) {
   return (
     <label>
       Search:{' '}
       <input
         value={query}
-        onChange={handleChange}
+        onChange={(e) => setQuery(e.target.value)}
       />
     </label>
   );
