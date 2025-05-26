2d0da9de5a9cce85a46efc43f1d80914c0da9ffc
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 17:53:51 +0600

    Update App.tsx

--- src/1/1_4_2/App.tsx
+++ src/1/1_4_2/App.tsx
@@ -1,31 +1,27 @@
-// 1_4_2 Extract information into an object 
-/* 
-  Извлеките URL изображения в объект person.
-*/
-
+//Извлеките URL изображения в объект person.
 const person = {
-    name: 'Gregorio Y. Zara',
-    theme: {
+  name: 'Gregorio Y. Zara',
+  imageUrl: 'GregorioYZara.jpg', // Вынесли URL в объект
+  theme: {
       backgroundColor: 'black',
       color: 'pink'
-    }
-  };
-  
-  export default function TodoList() {
-    return (
-      <div style={person.theme}>
-        <h1>{person.name}'s Todos</h1>
-        <img
-          className="avatar"
-          src="GregorioYZara.jpg"
-          alt="Gregorio Y. Zara"
-        />
-        <ul>
-          <li>Improve the videophone</li>
-          <li>Prepare aeronautics lectures</li>
-          <li>Work on the alcohol-fuelled engine</li>
-        </ul>
-      </div>
-    );
   }
-  
+};
+
+export default function TodoList() {
+  return (
+      <div style={person.theme}>
+          <h1>{person.name}'s Todos</h1>
+          <img
+              className="avatar"
+              src={person.imageUrl} // Используем переменную
+              alt={person.name}
+          />
+          <ul>
+              <li>Improve the videophone</li>
+              <li>Prepare aeronautics lectures</li>
+              <li>Work on the alcohol-fuelled engine</li>
+          </ul>
+      </div>
+  );
+}
