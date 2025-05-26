8e450fce3dafd92ba6a5da5bafa3c033bf835941
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 18:59:41 +0600

    Update App.tsx

--- src/4/4_2_2/App.tsx
+++ src/4/4_2_2/App.tsx
@@ -3,13 +3,22 @@
   Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
 */
 
+import { useRef } from 'react';
+
 export default function Page() {
+  const inputRef = useRef<HTMLInputElement>(null); // Реф для поля ввода
+
+  function handleClick() {
+    inputRef.current?.focus(); // Устанавливаем фокус
+  }
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={handleClick}>Search</button>
       </nav>
       <input
+        ref={inputRef} // Привязываем ref
         placeholder="Looking for something?"
       />
     </>
