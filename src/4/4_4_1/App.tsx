551282ae5441f856e9ee4ffb92379c60bc9a703b
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 19:04:13 +0600

    Update App.tsx

--- src/4/4_4_1/App.tsx
+++ src/4/4_4_1/App.tsx
@@ -1,3 +1,4 @@
+
 // 4_4_1 Transform data without Effects
 /*
   Приведенный ниже TodoList отображает список дел. Когда установлен флажок "Показывать только активные задания", завершенные задания не отображаются в списке. Независимо от того, какие из них видны, в нижнем колонтитуле отображается количество еще не завершенных дел.
@@ -5,31 +6,15 @@
   Упростите этот компонент, удалив все ненужные состояния и эффекты.
 */
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { initialTodos, createTodo, Todo } from './todos';
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
-  const [activeTodos, setActiveTodos] = useState<Todo[]>([]);
-  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
-  const [footer, setFooter] = useState<JSX.Element>(<></>);
 
-  useEffect(() => {
-    setActiveTodos(todos.filter(todo => !todo.completed));
-  }, [todos]);
-
-  useEffect(() => {
-    setVisibleTodos(showActive ? activeTodos : todos);
-  }, [showActive, todos, activeTodos]);
-
-  useEffect(() => {
-    setFooter(
-      <footer>
-        {activeTodos.length} todos left
-      </footer>
-    );
-  }, [activeTodos]);
+  const activeTodos = todos.filter(todo => !todo.completed);
+  const visibleTodos = showActive ? activeTodos : todos;
 
   return (
     <>
@@ -49,7 +34,7 @@ export default function TodoList() {
           </li>
         ))}
       </ul>
-      {footer}
+      <footer>{activeTodos.length} todos left</footer>
     </>
   );
 }
@@ -58,6 +43,7 @@ function NewTodo({ onAdd }: { onAdd: (todo: Todo) => void }) {
   const [text, setText] = useState('');
 
   function handleAddClick() {
+    if (text.trim() === '') return; // Защита от пустых задач
     setText('');
     onAdd(createTodo(text));
   }
@@ -65,10 +51,7 @@ function NewTodo({ onAdd }: { onAdd: (todo: Todo) => void }) {
   return (
     <>
       <input value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={handleAddClick}>
-        Add
-      </button>
+      <button onClick={handleAddClick}>Add</button>
     </>
   );
 }
-
