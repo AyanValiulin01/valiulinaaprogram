5407c606ca0ab219de1b999ccd58a8bad4126943
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 18:45:53 +0600

    Update Contact.tsx

--- src/3/3_4_5/Contact.tsx
+++ src/3/3_4_5/Contact.tsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
 import { ContactType } from './App';
 
-export default function Contact({ contact }: { contact: ContactType }) {
-  const [expanded, setExpanded] = useState(false);
+export default function Contact({ 
+  contact, 
+  expanded, 
+  onToggle 
+}: { 
+  contact: ContactType, 
+  expanded: boolean, 
+  onToggle: () => void 
+}) {
   return (
     <>
       <p><b>{contact.name}</b></p>
-      {expanded &&
-        <p><i>{contact.email}</i></p>
-      }
-      <button onClick={() => {
-        setExpanded(!expanded);
-      }}>
+      {expanded && <p><i>{contact.email}</i></p>}
+      <button onClick={onToggle}>
         {expanded ? 'Hide' : 'Show'} email
       </button>
     </>
