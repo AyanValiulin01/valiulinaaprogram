96ecd880bdd9578c587013752eb37f9e9dc263e5
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 17:52:20 +0600

    Update App.tsx

--- src/1/1_3_1/App.tsx
+++ src/1/1_3_1/App.tsx
@@ -1,17 +1,14 @@
-// 1_3_1 Convert some HTML to JSX
-/* 
-  Этот HTML был вставлен в компонент, но это не корректный JSX. Исправьте его.
-*/
 
+//Этот HTML был вставлен в компонент, но это не корректный JSX. Исправьте его.
 export default function Bio() {
     return (
-      <div class="intro">
-        <h1>Welcome to my website!</h1>
-      </div>
-      <p class="summary">
-        You can find my thoughts here.
-        <br><br>
-        <b>And <i>pictures</b></i> of scientists!
-      </p>
+        <div className="intro">
+            <h1>Welcome to my website!</h1>
+            <p className="summary">
+                You can find my thoughts here.
+                <br /><br />
+                <b>And <i>pictures</i></b> of scientists!
+            </p>
+        </div>
     );
-  }
+}
