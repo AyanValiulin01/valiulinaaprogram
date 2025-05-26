026cae2ee3c4a3f4951b73369b8e2383aaa58e19
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 18:47:36 +0600

    Update Chat.tsx

--- src/3/3_5_1/Chat.tsx
+++ src/3/3_5_1/Chat.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
 import { Action } from './messengerReducer';
 import { Contact } from './App';
 
 export default function Chat(
-    {
-        contact, message, dispatch
-    }:
-    {
-        contact: Contact,
-        message: string,
-        dispatch: (action: Action) => void
-    }
+    { contact, message, dispatch }:
+    { contact: Contact, message: string, dispatch: (action: Action) => void }
 ) {
   return (
     <section className="chat">
@@ -18,8 +11,10 @@ export default function Chat(
         value={message}
         placeholder={'Chat to ' + contact.name}
         onChange={(e) => {
-          // TODO: dispatch edited_message
-          // (Read the input value from e.target.value)
+          dispatch({ 
+            type: 'edited_message', 
+            message: e.target.value 
+          });
         }}
       />
       <br />
