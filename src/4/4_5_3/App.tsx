123b42f976d73a9cc521c09c161f7c26d65e54f9
AyanValiulin01 <ayangross@outlook.com>
2025-05-26 19:08:32 +0600

    Update App.tsx

--- src/4/4_5_3/App.tsx
+++ src/4/4_5_3/App.tsx
@@ -1,3 +1,4 @@
+
 // 4_5_3 Investigate a stale value bug
 /*
   В этом примере розовая точка должна двигаться, когда флажок включен, и прекращать движение, когда флажок выключен. Логика для этого уже реализована: обработчик события handleMove проверяет переменную состояния canMove.
@@ -11,24 +12,24 @@ export default function App() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [canMove, setCanMove] = useState(true);
 
-  function handleMove(e) {
-    if (canMove) {
-      setPosition({ x: e.clientX, y: e.clientY });
-    }
-  }
-
   useEffect(() => {
+    function handleMove(e: PointerEvent) {
+      if (canMove) {
+        setPosition({ x: e.clientX, y: e.clientY });
+      }
+    }
+
     window.addEventListener('pointermove', handleMove);
     return () => window.removeEventListener('pointermove', handleMove);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [canMove]); // Теперь useEffect обновляется при изменении canMove
 
   return (
     <>
       <label>
-        <input type="checkbox"
+        <input 
+          type="checkbox"
           checked={canMove}
-          onChange={e => setCanMove(e.target.checked)} 
+          onChange={e => setCanMove(e.target.checked)}
         />
         The dot is allowed to move
       </label>
